726e68efc78c1f1c5354440bef3700a90ba81932
kichi1986 <kichi.wonders@gmail.com>
2024-08-04 12:24:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from AWS & git & Jenkins !" );
+        System.out.println( "Hello Hello World from AWS & git & Jenkins !" );
     }
 }
